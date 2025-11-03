62cbd9c577c7de39aef6b51cf8a8e305783454af
Nokteholda <69749293+nokteholda@users.noreply.github.com>
2025-11-03 14:38:22 +0700

    Add files via upload

---
+++ Hidup Jokowi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void rekursif(int);
+int main(){
+	rekursif(0);
+}
+
+void rekursif(int loop){
+	cout << "HIDUP JOKOWI!\n";
+	loop++;
+	if(loop>=5){
+		return;
+	}else{
+		rekursif(loop);
+	}
+}
